b1aab9b1107aa58496dfc0845526fb871c927da8
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-05 12:43:27 -0300

    feat: throws not found error if product code doesnt exists on get single product requets

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,6 +1,8 @@
 import { findProduct } from '../repositories/productsService.js';
+import { notFoundError } from '../utils/errorUtils.js';
 
 export async function getProduct(code: string) {
   const singleProduct = await findProduct(code);
+  if (!singleProduct) throw notFoundError('Product code not found');
   return singleProduct;
 }
